a1af8dbd4d45ee05a31d3b26d9b042ea19ef866c
Adit <arnavle1998@gmail.com>
2024-05-26 14:57:54 +0530

    signIn/UpToggle

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import TogglePage from './Components/Auth/TogglePage'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <TogglePage/>
+      </div>
+    </>
+  )
+}
+
+export default App
